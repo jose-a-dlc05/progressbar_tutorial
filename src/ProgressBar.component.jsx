69c08cf72430827a844a8dde2f4e89bb6daaec51
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-09-30 16:12:13 -0400

    Added the styling to the component

--- src/ProgressBar.component.jsx
+++ src/ProgressBar.component.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	progress[value] {
+		width: ${(props) => props.width};
+		appearance: none;
+
+		::-webkit-progress-bar {
+			height: 10px;
+			border-radius: 20px;
+			background-color: #eee;
+		}
+
+		::-webkit-progress-value {
+			height: 10px;
+			border-radius: 20px;
+			background-color: ${(props) => props.color};
+		}
+	}
+`;
 
 const ProgressBar = (props) => {
-	const { value, max } = props;
+	const { value, max, color, width } = props;
 	return (
-		<div>
+		<Container color={color} width={width}>
 			<progress value={value} max={max} />
 			<span>{(value / max) * 100}%</span>
-		</div>
+		</Container>
 	);
 };
 
 ProgressBar.propTypes = {
 	value: PropTypes.number.isRequired,
 	max: PropTypes.number,
+	color: PropTypes.string,
+	width: PropTypes.string,
 };
 
 ProgressBar.defaultProps = {
 	max: 100,
+	color: '#ff7979',
+	width: '150px',
 };
 
 export default ProgressBar;

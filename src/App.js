467676c8ffc8b1d09284a394b87980de28b1cc23
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-09-30 16:00:07 -0400

    render the percentage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import ProgressBar from './ProgressBar.component';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	const [value, setValue] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			// Run this every second
+			setValue((oldValue) => {
+				// update the value by 10
+				const newValue = oldValue + 10;
+
+				if (newValue === 100) {
+					// If the value hits 100, clear the interval
+					clearInterval(interval);
+				}
+
+				return newValue;
+			});
+		}, 1000);
+	}, []);
+	return <ProgressBar value={value} />;
 }
 
 export default App;
